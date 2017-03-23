188f6bb642963de44dfef6cd4e1992978753701b
master <master.example.com>
2017-03-23 17:17:06 +0900

    スタッフクラスの作成

---
+++ ShogiStaff.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+class ShogiStaff {
+
+    private init() {
+        // init
+    }
+
+    func ready_to_shogi() {
+        // todo
+    }
+
+    func ready_to_player(myself : String, yourself : String) {
+        // todo
+    }
+
+    func start() {
+        // todo
+    }
+
+    func end() -> Bool {
+        // todo
+    }
+
+    func switch_player() {
+        // todo
+    }
+
+    func write_to_result() {
+        // todo
+    }
+
+    func write_to_shogikifu() {
+        // todo
+    }
+
+    func remove_to_shogi() {
+        // todo
+    }
+}
