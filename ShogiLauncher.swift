418ffbc9a587071a7585c7092dbc767c67b951bb
master <master.example.com>
2017-03-23 17:15:46 +0900

    Launcherクラスの修正

--- ShogiLauncher.swift
+++ ShogiLauncher.swift
@@ -1,4 +1,4 @@
-import Foundation;
+import Foundation
 
 class ShogiLauncher {
 
@@ -7,30 +7,27 @@ class ShogiLauncher {
     }
 
     func main() {
-        var staff = ShogiStaff();
-        var playerA = ShogiPlayer(name : "A");
-        var playerB = ShogiPlayer(name : "B");
+        var staff = ShogiStaff()
+        var playerA = ShogiPlayer(name : "A")
+        var playerB = ShogiPlayer(name : "B")
 
         // ready phase
-        staff.ready_to_shogi();
-        staff.ready_to_player(myself : playerA, yourself : playerB);
+        staff.ready_to_shogi()
+        staff.ready_to_player(myself : playerA, yourself : playerB)
 
         // game phase
-        staff.start();
+        staff.start()
         while staff.end() {
-            var player = staff.switch_player();
+            var player = staff.switch_player()
             if player.te() = false {
-                staff.write_to_result();
+                staff.write_to_result()
                 break
             }
-            player.stop_shogitimer();
-            staff.write_to_shogikifu();
+            player.stop_shogitimer()
+            staff.write_to_shogikifu()
         }
 
         // end phase
-        staff.remove_to_shogi();
+        staff.remove_to_shogi()
     }
 }
-
-var launcher = ShogiLauncher();
-launcher.main();
