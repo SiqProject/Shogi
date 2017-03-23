0fcd9937e1a661990ad1f7194708fa1e12cc81cc
master <master.example.com>
2017-03-23 17:16:39 +0900

    プレイヤークラスの作成

---
+++ ShogiPlayer.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class ShogiPlayer {
+
+    var Name : String
+
+    private init(name : String) {
+        Name = name
+    }
+
+    func te() {
+        // todo
+    }
+
+    func stop_shogitimer() {
+        // todo
+    }
+}
